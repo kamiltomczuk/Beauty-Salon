52cd71b234208bf7339de32e9ba2c67943645193
Kamil Tomczuk <91956660+kamiltomczuk@users.noreply.github.com>
2023-06-11 20:50:46 +0200

    Update config.php

--- loginregister/config.php
+++ loginregister/config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'mysql.ct8.pl');
-define('DB_USERNAME', 'm29582_ktpjatk');
-define('DB_PASSWORD', 'Siema123');
-define('DB_NAME', 'm29582_test');
+define('DB_SERVER', '##### ');
+define('DB_USERNAME', '#####');
+define('DB_PASSWORD', '#####');
+define('DB_NAME', '#####');
 
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
